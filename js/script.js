87755207dfcb1d68464f0c028a20a77c7a7aad29
Evgeny Kolesnikov <zhenyadope@gmail.com>
2020-09-20 18:01:11 +0300

    Сверстана секция с ценами и добавлен слайдер

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log('working')
+
+// Количество карточек которые будут оборажаться в слайдере с ценами
+let priceCardValue = 3;
+
+
+// Слайдер цен
+$('.slider-wrapper').slick({
+    slidesToShow: priceCardValue,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: true,
+  });
+
+
+  
