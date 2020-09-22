92383d374fa940a8f805aac560a42f1bb149b623
Evgeny Kolesnikov <zhenyadope@gmail.com>
2020-09-22 17:33:21 +0300

    добавлено модальное окно

--- js/script.js
+++ js/script.js
@@ -11,3 +11,23 @@ $('.slider-wrapper').slick({
     autoplaySpeed: 3000,
     arrows: true,
 });
+
+
+
+// Модальное окно
+
+const btnOpenModal = document.getElementById('openModal');
+const btnCloseModal = document.getElementById('closeModal');
+const modalWindow = document.getElementById('modalForm');
+
+const openModal = () => {
+    modalWindow.style.left = '0px';
+}
+
+const closeModal = () => {
+    modalWindow.style.left = '-5000px';
+}
+
+
+btnOpenModal.addEventListener('click', openModal);
+btnCloseModal.addEventListener('click',closeModal);
